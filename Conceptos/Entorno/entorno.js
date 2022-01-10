f3ed8045fc1523affd79c3157e0b29f442be28da
HansPuente <hpuentej@uni.pe>
2022-01-10 07:25:06 -0500

    Cree .env en la raíz, no pasó nada

--- Conceptos/Entorno/entorno.js
+++ Conceptos/Entorno/entorno.js
@@ -1,13 +1,18 @@
 //Ejemplo 1
 
-let nombre = process.env.NOMBRE || 'Sin nombre';
-let web = process.env.WEB_JUNIOR || 'No tengo Web';
+// let nombre = process.env.NOMBRE || 'Sin nombre';
+// let web = process.env.WEB_JUNIOR || 'No tengo Web';
 
-console.log(`Hola ${nombre}`);
-console.log(`Mi web es ${web}`);
+// console.log(`Hola ${nombre}`);
+// console.log(`Mi web es ${web}`);
 
 /*Llamado en node (terminal):
 NOMBRE=Pamela WEB_JUNIOR=Inicio2 node Conceptos/entorno.js*/
 
 //Ejemplo 2
 
+const lista = process.env.MI_LISTA.split(',') ?? 'Lista vacia';
+console.log(lista);
+
+/*Llamado en node (terminal):
+MI_LISTA='abc' MI_LISTA=a,b,c node Conceptos/Entorno/entorno.js*/
--- Conceptos/Entorno/entornoFile.js
+++ Conceptos/Entorno/entornoFile.js
@@ -1,2 +1,2 @@
 require('dotenv').config();
-console.log(process.env.EMAIL);
+console.log(process.env.NAME);
