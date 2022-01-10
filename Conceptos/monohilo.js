3d26768a2ec161ac0fb493fcefb5e3085650fab2
HansPuente <hpuentej@uni.pe>
2022-01-10 04:44:37 -0500

    Concepto de monohilo en nodejs

---
+++ Conceptos/monohilo.js
@@ -0,0 +1,11 @@
+ console.log('Hola mundo');
+ let i=0;
+ setInterval(function(){
+     console.log(i);
+     i+=1;
+     if(i===5){ 
+         var a = 3+z;
+     }
+ },1000);
+
+console.log('Segunda instrucción')
